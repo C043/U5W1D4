50688e4bdf85ad954b0490f1c1b93ee6a3f32fcf
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-05 18:53:23 +0200

    Removed code

--- src/main/java/U5W1D4/OrderRunner.java
+++ src/main/java/U5W1D4/OrderRunner.java
@@ -22,21 +22,7 @@ public class OrderRunner implements CommandLineRunner {
         Bevanda acqua = (Bevanda) context.getBean("acqua");
         Topping tomato = (Topping) context.getBean("tomato");
         Topping cheese = (Topping) context.getBean("cheese");
-/*
-        Menu menu = context.getBean(Menu.class);
-        menu.printMenu();
-*/
 
-/*
-            List<Cibo> ciboList = new ArrayList<>();
-            ciboList.add((Cibo) context.getBean("pizza_margherita"));
-            ciboList.add((Cibo) context.getBean("salami_pizza"));
-            ciboList.add((Cibo) context.getBean("cheese"));
-            ciboList.add((Cibo) context.getBean("birra"));
-            ciboList.add((Cibo) context.getBean("vino"));
-            Ordine ordine = new Ordine((Tavolo) context.getBean("tavolo2"), 1, ciboList, 2, (double)context.getBean("coperto"));
-            ordine.printOrder();
-*/
         try {
             ciboService.saveFood(tomato);
             ciboService.saveFood(cheese);
